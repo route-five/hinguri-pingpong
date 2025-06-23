a3337e5433faa12edf5bc8adf418a54747cfa028
Cyan <hoony6134@gmail.com>
2025-06-23 18:15:31 +0900

    feat: Object detection base

---
+++ src/object_detection.cpp
@@ -0,0 +1,26 @@
+//
+// Created by 임정훈 on 25. 6. 23.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#define WINDOW_NAME "example_window"
+
+int main() {
+    cv::Mat source = cv::imread("pingpong.jpg");
+
+    if (source.empty()) {
+        std::cerr << "Failed to load image" << std::endl;
+        return -1;
+    }
+
+    imshow("source", source);
+    cv::Mat mask;
+    cv::Scalar lowerb = cv::Scalar(33, 120, 200); //BGR, needs to be changed hueristically
+    cv::Scalar upperb = cv::Scalar(90, 180, 280); //BGR
+    cv::inRange(source, lowerb, upperb, mask);
+
+    cv::imshow(WINDOW_NAME, mask);
+    cv::waitKey(0);
+    cv::destroyWindow(WINDOW_NAME);
+}
