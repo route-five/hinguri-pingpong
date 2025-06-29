c0c9ead1a4d06dac7ce2c6e0c469ef9e6948aa29
Cyan <hoony6134@gmail.com>
2025-06-29 15:01:42 +0900

    refactor: object detection constants to Constants.hpp

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -5,7 +5,11 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
+#include <opencv2/opencv.hpp>
+
 constexpr float TABLE_WIDTH = 152.5;
 constexpr float TABLE_HEIGHT = 274;
+constexpr cv::Scalar ORANGE_MIN = cv::Scalar(33, 120, 200); //BGR
+constexpr cv::Scalar ORANGE_MAX = cv::Scalar(90, 180, 280); //BGR
 
 #endif //CONSTANTS_HPP
