3215c63c06e3745adfc97d9c3c6c00c9ef88d48e
Cyan <65375075+hoony6134@users.noreply.github.com>
2025-07-08 18:41:29 +0900

    chore: unsigned int for sleep functions
    Co-authored-by: Hyunseung Ryu <rhseungg@gmail.com>

--- src/utils/sleep.hpp
+++ src/utils/sleep.hpp
@@ -1,10 +1,10 @@
 #include <thread>
 #include <chrono>
 
-void sleep_ms(int ms) {
+void sleep_ms(unsigned int ms) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-void sleep_s(int s) {
+void sleep_s(unsigned int s) {
 	std::this_thread::sleep_for(std::chrono::seconds(s));
 }
