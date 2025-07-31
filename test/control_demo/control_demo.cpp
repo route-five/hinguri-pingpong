65e32f88f52992a7016ab2fa80e28cc6aaa273f6
Hyunseung Ryu <rhseungg@gmail.com>
2025-08-01 00:27:20 +0900

    fix: dynamixel swing issue

--- test/control_demo/control_demo.cpp
+++ test/control_demo/control_demo.cpp
@@ -2,6 +2,11 @@
 #include "control/linear_actuator.hpp"
 #include "vision/bridge.hpp"
 #include "utils/constants.hpp"
+#include <thread>
+#include <mutex>
+#include <optional>
+#include <atomic>
+#include <iostream>
 
 inline std::mutex mutex;
 inline std::atomic<bool> stop_all{ false };
@@ -27,46 +32,55 @@ public:
             return;
         }
         while (!stop_all.load()) {
-            Bridge::Payload payload;    
+            Bridge::Payload payload;
             {
                 std::lock_guard lock(mutex);
                 if (!shared_payload.has_value())
                     continue;
-
-                payload = shared_payload.value();
+                payload = *shared_payload;
                 shared_payload.reset();
 
-                std::cout << "Received from vision: " << std::endl
-                    << "\tx: " << payload.x << " cm" << std::endl
-                    << "\ttheta: " << payload.theta << " deg" << std::endl
-                    << "\tswing_start: " << payload.swing_start << " deg" << std::endl
-                    << "\tswing_end: " << payload.swing_end << " deg" << std::endl
-                    << "\twrist_angle: " << payload.wrist_angle << " deg" << std::endl
-                    << "=========================" << std::endl;
+                std::cout << "Received from vision:\n"
+                    << "\tx: " << payload.x << " cm\n"
+                    << "\ttheta: " << payload.theta << " deg\n"
+                    << "\tswing_start: " << payload.swing_start << " deg\n"
+                    << "\tswing_end: " << payload.swing_end << " deg\n"
+                    << "\twrist_angle: " << payload.wrist_angle << " deg\n"
+                    << "=========================\n";
             }
-
             execute(payload);
         }
-
         shutdown();
     }
 
 private:
     BulkDynamixelActuator actuators;
-    LinearActuator linearActuator;
+    LinearActuator        linearActuator;
 
     void execute(const Bridge::Payload& payload) {
         const auto& [x, theta, swing_start, swing_end, wrist_angle, use_right_hand] = payload;
 
         const float top_target = wrist_angle;
         const float mid_target = theta;
-        float bot_target = swing_start;
 
-        actuators.bulk_move_by_degrees({ top_target, mid_target, bot_target });
+        // 1) Move to swing_start and wait
+        bool swing_start_done = false;
+        actuators.move_and_wait_by_degrees(
+            { top_target, mid_target, swing_start },
+            &swing_start_done
+        );
+        // swing_start_done == true here
+
+        // 2) Meanwhile move the linear actuator
         linearActuator.move_actu(x);
 
-        bot_target = swing_end;
-        actuators.bulk_move_by_degrees({ top_target, mid_target, bot_target });
+        // 3) Only after swing_start is done, move to swing_end and wait
+        bool swing_end_done = false;
+        actuators.move_and_wait_by_degrees(
+            { top_target, mid_target, swing_end },
+            &swing_end_done
+        );
+        // swing_end_done == true here
     }
 
     void shutdown() {
@@ -86,31 +100,17 @@ int main() {
         std::this_thread::sleep_for(std::chrono::seconds(2));
 
         float x, theta, swing_start, swing_end, wrist_angle;
-        
-        std::cout << "x: ";
-        std::cin >> x;
-        if (x == -1000) {
-            stop_all = true;
-            break;
-        }
+        std::cout << "x: ";           std::cin >> x;
+        if (x == -1000) { stop_all = true; break; }
 
-        std::cout << "theta: ";
-        std::cin >> theta;
+        std::cout << "theta: ";      std::cin >> theta;
+        std::cout << "swing_start: "; std::cin >> swing_start;
+        std::cout << "swing_end: ";  std::cin >> swing_end;
+        std::cout << "wrist_angle: "; std::cin >> wrist_angle;
 
-        std::cout << "swing_start: ";
-        std::cin >> swing_start;
-        
-        std::cout << "swing_end: ";
-        std::cin >> swing_end;
-
-        std::cout << "wrist_angle: ";
-        std::cin >> wrist_angle;
-        
-        Bridge::Payload payload(x, theta, swing_start, swing_end, wrist_angle);
-        shared_payload = payload;
+        shared_payload = Bridge::Payload{ x, theta, swing_start, swing_end, wrist_angle };
     }
 
     control_thread.join();
-
     return 0;
 }
